c4d7093f223a037c4422cfde4b3a48aa84b7724d
kyuran kim | fry <57716832+gxxrxn@users.noreply.github.com>
2023-03-02 22:43:09 +0900

    [level 2] Title: 피보나치 수, Time: 4.87 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    return fibonumber(n) % 1234567;
+}
+
+function fibonumber(v) {
+    let bottom_up = [];
+    
+    bottom_up[0] = 0;
+    bottom_up[1] = 1;
+    bottom_up[2] = 1;
+    for (let i = 3; i <= v; i++) {
+        bottom_up[i] = (bottom_up[i-1]%1234567 + bottom_up[i-2]%1234567);
+    }
+    return bottom_up[v];
+}
